6b336185d37e59c119fc48b138f853a426880e60
Lanai Conceição <conceicao.lanai@gmail.com>
2022-02-04 18:23:21 -0300

    Criando método POST na rota /user

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv/config');
+const User = require('./controllers/User');
+const validateUser = require('./controllers/middlewares/validateUserMiddleware');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.post('/user',
+  validateUser.validateName,
+  validateUser.validateEmail,
+  validateUser.validatePassword,
+  User.add);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 

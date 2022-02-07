1d9ed7bba1523262b0bcc823d556a17db0181e7b
Lanai Conceição <conceicao.lanai@gmail.com>
2022-02-07 16:33:35 -0300

    Requisito 5: Seeders criadas + Model criado

---
+++ models/Category.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const Categories = sequelize.define(
+    'Categories',
+    {
+      name: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+    },
+  );
+
+  return Categories;
+};

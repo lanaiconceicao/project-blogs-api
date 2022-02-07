8ac003a6ee6826c0b2f8559ecbfa33c8f3745d04
Lanai Conceição <conceicao.lanai@gmail.com>
2022-02-07 15:32:38 -0300

    Refatorando para melhorar os nomes dos arquivos

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,23 +1,23 @@
 const rescue = require('express-rescue');
 
-const User = require('../services/User');
+const UserService = require('../services/UserService');
 
 const add = rescue(async (req, res) => {
   const { displayName, email, password, image } = req.body;
 
-  const user = await User.add(displayName, email, password, image);
+  const user = await UserService.add(displayName, email, password, image);
 
   res.status(201).json(user);
 });
 
 const getAll = rescue(async (_req, res) => {
-  const getAllUsers = await User.getAll();
+  const getAllUsers = await UserService.getAll();
   res.status(200).json(getAllUsers);
 });
 
 const getById = rescue(async (req, res) => {
   const { id } = req.params;
-  const getUserById = await User.getById(id);
+  const getUserById = await UserService.getById(id);
   
   if (!getUserById) return res.status(404).json({ message: 'User does not exist' });
   

778e0761caa7b6c68e9a39e05255b92e64ae7ff8
Lanai Conceição <conceicao.lanai@gmail.com>
2022-02-07 18:38:12 -0300

    Requisito 7: Migration criada

---
+++ migrations/20220207213229-BlogPosts.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(
+      'BlogPosts',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        title: {
+          type: Sequelize.STRING,
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+          references: { model: 'Users', key: 'id' },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.fn('now'),
+          field: 'published',
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.fn('now'),
+          field: 'updated',
+        },
+      },
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('BlogPosts', null, {});
+  }
+};

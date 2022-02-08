c07a5ef94dafc9e4a1e16c4b0c6daed776b507df
Lanai Conceição <conceicao.lanai@gmail.com>
2022-02-08 10:59:54 -0300

    Requisito 7: Model feito

---
+++ models/BlogPost.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPosts = sequelize.define(
+    'BlogPosts',
+    {
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      userId: { type: DataTypes.INTEGER, foreignKey: true },
+      published: { type: DataTypes.DATE },
+      updated: { type: DataTypes.DATE },
+    },
+    {
+      timestamps: false,
+    },
+  );
+
+  BlogPosts.associate = (models) => {
+    // Tipo de relacionamento: 1:N
+    // BlogPosts PERTENCE à tabela Users, onde a foreign key é a userId
+    BlogPosts.belongsTo(models.Users, { foreignKey: 'userId', as: 'user' });
+  };
+
+  return BlogPosts;
+};

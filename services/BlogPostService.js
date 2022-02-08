de6b7a69521888e6a3aafb16871676d03ef7728d
Lanai Conceição <conceicao.lanai@gmail.com>
2022-02-08 11:49:52 -0300

    Requisito 7: Blogpost Service criado

---
+++ services/BlogPostService.js
@@ -0,0 +1,18 @@
+const { BlogPosts } = require('../models/BlogPost');
+
+const add = async (title, content, userData) => {
+  const userId = userData.id;
+  console.log(userId, 'userId no BlogpostService');
+  const posts = await BlogPosts.create({ title, content, userId });
+
+  return {
+    id: posts.id,
+    title: posts.title,
+    content: posts.content,
+    userId,
+  };
+};
+
+module.exports = {
+  add,
+};

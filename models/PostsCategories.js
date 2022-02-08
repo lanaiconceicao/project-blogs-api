fd43fce96097ef6354f708c1721b42a81c1a261b
Lanai Conceição <conceicao.lanai@gmail.com>
2022-02-08 17:18:57 -0300

    Requisito 8: Model criado

---
+++ models/PostsCategories.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, _DataTypes) => {
+  const PostsCategories = sequelize.define('PostsCategories', {}, { timestamps: false });
+  // Tabela de associação
+
+  PostsCategories.associate = (models) => {
+    // Relacionamento N:N
+    // O model BlogPosts PERTENCE A MUITOS
+    models.BlogPosts.belongsToMany(
+      // 'MUITOS' nesse caso é a tabela Categories
+      models.Categories, { as: 'categories',
+      // "na chave through estamos informando qual o Model que servira como tabela de associação"
+      // https://tableless.com.br/sequelize-a-solu%C3%A7%C3%A3o-para-seus-relacionamentos/
+        through: PostsCategories,
+        foreignKey: 'postId',
+        otherKey: 'categoryId',
+      },
+    );
+
+    models.Categories.belongsToMany(
+      models.BlogPosts, { as: 'posts',
+        through: PostsCategories,
+        foreignKey: 'categoryId',
+        otherKey: 'postId',
+      },
+    );
+  };
+
+  return PostsCategories;
+};
